d18b43ee7eaace2b7690a37b02e6e4409f04941a
Manvir Singh <gurs1kh@live.com>
2020-11-03 17:40:20 -0800

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import ReactPrismEditor from "react-prism-editor";
 
 function App() {
+  const [ code, setCode ] = useState('');
+  const [ verses, setVerses ] = useState([]);
+
+  function updateCode(code) {
+    setCode(code);
+    try {
+      if (code[0] != '[') code = `[${code}]`;
+      code = code.replace(/\^(\d)/g, (match, d) => '⁰¹²³⁴⁵⁶⁷⁸⁹'[+d]);
+      let verses = eval(code);
+      setVerses(verses);
+    } catch (e) {
+      console.error(`${e}\n${code}`)
+    };
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="json-editor">
+        <ReactPrismEditor
+          language="javascript"
+          theme="default"
+          lineNumber={true}
+          code={code}
+          changeCode={newCode => updateCode(newCode)}
+          height='100vw'
+        />
+      </div>
+      <div className="rendered-view">
+        {verses.map((verse, i) => <Verse verse={verse} key={i} />)}
+      </div>
+    </div>
+  );
+}
+
+function Verse({ verse }) {
+  const className = [
+    'gurmukhi',
+    verse.title && 'title',
+    verse.sirlekh && 'sirlekh',
+  ].filter(d => d).join(' ');
+
+  let padArth = Object.entries(verse.padArth || {}).map(([key, arth]) => {
+    return (
+      <span key={key}>
+        {key}
+        <span className='punjabi'>{arth.punjabi}</span>{' '}
+        <span className='english'>{arth.english}</span>{' '}
+      </span>
+    );
+  });
+
+  return (
+    <div className='verse'>
+      <p className={className}>{verse.gurmukhi}</p>
+      <div className='pad-arth'>
+         <p>{padArth}</p>
+      </div>
+      <div className='arth'>
+        <p className='punjabi'>{verse.arth?.punjabi}</p>
+        <p className='english'>{verse.arth?.english}</p>
+      </div>
     </div>
   );
 }
